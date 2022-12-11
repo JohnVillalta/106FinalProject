c3ca41ac9ddbb021a0ad7107634b818acbbe041c
JohnVillalta <jvillalta28007@gmail.com>
2022-12-11 11:08:15 -0800

    updates to most main pages, allows functionality to use search bar

--- app.py
+++ app.py
@@ -4,20 +4,22 @@ from flask import (
     request,
     session,
     url_for,
-    redirect
+    redirect,
+    g
 )
 
 app = Flask(__name__)
-nameT = ''
 
+#this is the home page landing, this page gives you access to refreshing home page, accessing your profile if logged in
+#logging in, and searching for other account profiles
 @app.route('/home', methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST' and request.form.get('home') == 'Home':
+    if request.method == 'POST' and request.form.get('home') == 'Home': #refresh home page
         return redirect(url_for('home'))
-    if request.method == 'POST' and request.form.get('profile') == 'Profile':
+    if request.method == 'POST' and request.form.get('profile') == 'Profile': #access your own profile
         return redirect(url_for('profileSelf'))
-    if request.method == 'POST' and request.form.get('search') == 'Search':
-        nameT = request.form.get('searchBar')
+    if request.method == 'GET' and request.args.get('search') != None: #access another profile and use search bar arguement as nameT
+        nameT = request.args.get('searchBar')
         return redirect(url_for('profileOther', nameT=nameT))
 
     return render_template('home.html')
@@ -28,17 +30,22 @@ def login():
     # if request.method == 'POST':
     return render_template('login.html')
 
-@app.route('/profile-other', methods=['GET', 'POST'])
-def profileOther():
-    if request.method == 'POST' and request.form.get('home') == 'Home':
+#this must always have nameT as a parameter since we're using that to find the profile, future implementation could use
+#user.ID or something of the sort :)
+@app.route('/profile-other/<nameT>', methods=['GET', 'POST'])
+def profileOther(nameT):
+    if request.method == 'POST' and request.form.get('home') == 'Home': #redirect to home page landing
         return redirect(url_for('home'))
+    if request.method == 'POST' and request.form.get('profile') == 'Profile': #redirect to your own page if signed in
+        return redirect(url_for('profileSelf'))
     return render_template('profileOther.html', nameT=nameT)
 
+#directs you to your own profile if logged in, otherwise will lead to login page
 @app.route('/profile-self', methods=['GET', 'POST'])
 def profileSelf():
-    if request.method == 'POST' and request.form.get('home') == 'Home':
+    if request.method == 'POST' and request.form.get('home') == 'Home': #redirect to home page landing
         return redirect(url_for('home'))
-    if request.method == 'POST' and request.form.get('profile') == 'Profile':
-        return redirect(url_for('profile'))
+    if request.method == 'POST' and request.form.get('profile') == 'Profile': #refresh to your own page is signed in
+        return redirect(url_for('profileSelf'))
 
     return render_template('profile.html')
